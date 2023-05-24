748e6bed54111765b5a3bba7ee114ec8fa25470f
MGZDRMR <134469449+MGZDRMR@users.noreply.github.com>
2023-05-23 21:49:01 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ficheros1
+{
+    //Manuel Angel Gómez Caballero
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                StreamReader sr = new StreamReader("./archivo.csv" + "");
+
+                string linea;
+
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(linea);
+                }
+
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("No se ha encontrado el archivo");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
